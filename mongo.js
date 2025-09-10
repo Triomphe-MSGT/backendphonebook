fb0cac8ac2e666b6f907115bc0b0a861da6c2787
Triomphe-MSGT <gregoiremandeng@gmail.com>
2025-09-10 19:17:54 +0100

    backend de note mis a jour

--- mongo.js
+++ mongo.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-console.log('give password as argument')
-process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://Mookdb:${password}@cluster0.upxs4sz.mongodb.net/Notedb?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://Mookdb:${password}@cluster0.upxs4sz.mongodb.net/testNotedb?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 const noteSchema = new mongoose.Schema({
-content: String,
-important: Boolean,
+  content: String,
+  important: Boolean,
 })
 const Note = mongoose.model('Note', noteSchema)
 const note = new Note({
-content: 'HTML is easy',
-important: true,
+  content: 'HTML is eas',
+  important: true,
 })
-note.save().then(result => {
-console.log('note saved!')
-mongoose.connection.close()
+note.save().then(() => {
+  console.log('note saved!')
+  mongoose.connection.close()
 })
